0154e6753476ae22fe162b12b2e2a5e7481b0fe5
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-01-08 18:52:53 -0300

    Select and play song in player
    Buttons play and player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { assets, songsData } from "../assets/assets";
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
+  const { track, seekbg, seekBar, playStatus, play, pause } =
+    useContext(PlayerContext);
+
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
-        <img className="w-12" src={songsData[0].image} alt="Song data" />
+        <img className="w-12" src={track.image} alt="Song data" />
         <div>
-          <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0, 12)}</p>
+          <p>{track.name}</p>
+          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
       <div className="flex flex-col items-center gap-1 m-auto">
@@ -23,11 +27,21 @@ const Player = () => {
             src={assets.prev_icon}
             alt="Previous Icon"
           />
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.play_icon}
-            alt="Play Icon"
-          />
+          {playStatus ? (
+            <img
+              onClick={pause}
+              className="w-4 cursor-pointer"
+              src={assets.pause_icon}
+              alt="Pause Icon"
+            />
+          ) : (
+            <img
+              onClick={play}
+              className="w-4 cursor-pointer"
+              src={assets.play_icon}
+              alt="Play Icon"
+            />
+          )}
           <img
             className="w-4 cursor-pointer"
             src={assets.next_icon}
@@ -41,8 +55,14 @@ const Player = () => {
         </div>
         <div className="flex items-center gap-5">
           <p>1:06</p>
-          <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-            <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
+          <div
+            ref={seekbg}
+            className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
+          >
+            <hr
+              ref={seekBar}
+              className="h-1 border-none w-0 bg-green-800 rounded-full"
+            />
           </div>
           <p>3:20</p>
         </div>
